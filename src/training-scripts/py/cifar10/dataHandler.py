c8355b106e5122c5d16de051b8259165c4bb4ff6
s-elo <zz321yy@qq.com>
2021-12-15 13:28:00 +0800

    add data agument

--- src/training-scripts/py/cifar10/dataHandler.py
+++ src/training-scripts/py/cifar10/dataHandler.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from random import shuffle
 from PIL import Image
 from tensorflow.keras.utils import to_categorical
 
@@ -37,6 +38,18 @@ def load_data(type='TRAIN'):
 
             imgs.append(img_vector)
             labels.append(classIdx)
+            
+    x = np.array(imgs).reshape(len(imgs), IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL)
+    y = to_categorical(labels, num_classes=CLASS_NUM)
 
     # return (np.array(imgs).reshape(len(imgs), IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), np.array(labels))
-    return (np.array(imgs).reshape(len(imgs), IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), to_categorical(labels, num_classes=CLASS_NUM))
+    return shuffle_dataset(x, y)
+
+
+def shuffle_dataset(x, y):
+    ind_list = [i for i in range(y.shape[0])]
+    shuffle(ind_list)
+    x_new = x[ind_list, :, :, :]
+    y_new = y[ind_list, ]
+
+    return (x_new, y_new)
--- src/training-scripts/py/cifar10/main.py
+++ src/training-scripts/py/cifar10/main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # from tensorflow.keras.datasets import cifar10
 # from tensorflow.keras.utils import to_categorical
 from dataHandler import load_data, CLASS_NUM
@@ -28,6 +29,8 @@ print('Data loaded.')
 # print(train_imgs[0:1], train_labels.shape)
 
 KERNEL_SIZE = 3
+BATCH_SIZE = 256
+EPOCH = 100
 
 
 def getModel():
@@ -63,16 +66,16 @@ def getModel():
     # flatten as one dimension
     model.add(layers.Flatten())
 
-    model.add(layers.Dropout(0.5))
-
     # fully connected layer 500 neurons
-    # model.add(layers.Dense(500, activation='relu'))
+    model.add(layers.Dense(units=128, activation='relu'))
 
-    # final fully connected layer 26 neurons with respect to 26 subjects
-    model.add(layers.Dense(CLASS_NUM, activation='softmax',
+    model.add(layers.Dropout(0.5))
+    
+    # final fully connected layer CLASS_NUM neurons with respect to CLASS_NUM subjects
+    model.add(layers.Dense(units=CLASS_NUM, activation='softmax',
               kernel_initializer='he_normal'))
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
@@ -80,16 +83,61 @@ def getModel():
 
 
 def train():
+    gen = dataAugment(train_imgs, train_labels, batch_size=BATCH_SIZE)
+
     model = getModel()
 
     model.summary()
 
     log_dir = "./logs/fit/"
+
     tensorboard_callback = tf.keras.callbacks.TensorBoard(
         log_dir=log_dir, histogram_freq=1)
 
-    model.fit(train_imgs, train_labels, epochs=100, batch_size=256, shuffle=True,
-              validation_data=(test_imgs, test_labels), callbacks=[tensorboard_callback])
+    def scheduler(epoch, lr):
+        if (epoch == 1):
+            return 0.01
+        if epoch % 10 == 0:
+            return lr - 0.0005
+        else:
+            return lr
+
+    learn_scheduler_callback = tf.keras.callbacks.LearningRateScheduler(
+        scheduler)
+
+    h = model.fit(x=gen,  epochs=EPOCH, steps_per_epoch=50000 // BATCH_SIZE,
+                  validation_data=(test_imgs, test_labels), callbacks=[tensorboard_callback])
+    # steps_per_epoch=50000//BATCH_SIZE
+
+    # model.fit(train_imgs, train_labels, epochs=EPOCH, batch_size=BATCH_SIZE, shuffle=True,
+    #           validation_data=(test_imgs, test_labels),
+    #           callbacks=[tensorboard_callback])
+
+    # model.save('CIFAR10_model_with_data_augmentation_dual_GPU.h5')
+    # validation_data=(test_imgs, test_labels),
+
+
+def dataAugment(x, y, batch_size=256):
+    aug_gen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+    )
+
+    aug_gen.fit(x)
+    gen = aug_gen.flow(x, y, batch_size=batch_size)
+
+    return gen
 
 
 train()
