1dc78bc4bcd2fbebc525e08c8458c4b785eb8b33
s-elo <zz321yy@qq.com>
2021-12-16 13:10:53 +0800

    add test split data train; add l2 regularization in Dense

--- src/training-scripts/py/cifar10/dataHandler.py
+++ src/training-scripts/py/cifar10/dataHandler.py
@@ -3,6 +3,7 @@ import numpy as np
 from random import shuffle
 from PIL import Image
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 TRAIN_PATH = './dataset/train'
 TEST_PATH = './dataset/test'
@@ -38,12 +39,86 @@ def load_data(type='TRAIN'):
 
             imgs.append(img_vector)
             labels.append(classIdx)
-            
+
     x = np.array(imgs).reshape(len(imgs), IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL)
     y = to_categorical(labels, num_classes=CLASS_NUM)
 
     # return (np.array(imgs).reshape(len(imgs), IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), np.array(labels))
     return shuffle_dataset(x, y)
+
+
+def load_split_train_data():
+    data_path = TRAIN_PATH
+    dataset = []
+
+    # initialize the dataset array
+    for _ in range(0, 5):
+        imgs = []
+        labels = []
+        dataset.append((imgs, labels))
+
+    # start to split
+    for classIdx in range(0, CLASS_NUM):
+        imgNames = os.listdir(data_path + '/' + CLASS_NAMES[classIdx])
+
+        imgIdx = 0
+        # 0-4
+        batchIdx = 0
+        while (imgIdx < len(imgNames)):
+            # every 1000 imgs in the current class are stored in a new batch
+            if (imgIdx % 1000 == 0 and imgIdx != 0):
+                batchIdx += 1
+
+            # get one image
+            img = Image.open(data_path + '/' +
+                             CLASS_NAMES[classIdx] + '/' + imgNames[imgIdx]).resize((IMG_WIDTH, IMG_HEIGHT))
+
+            # convert into a vector
+            img_vector = np.array(img).reshape(
+                IMG_WIDTH*IMG_HEIGHT*IMG_CHANNEL)/255
+            
+            # add to the current batch
+            dataset[batchIdx][0].append(img_vector)
+            dataset[batchIdx][1].append(classIdx)
+
+            imgIdx += 1
+
+    # reconstruct the batches
+    for batchIdx in range(0, 5):
+        cur_batch_imgs = dataset[batchIdx][0]
+        cur_batch_labels = dataset[batchIdx][1]
+
+        x = np.array(cur_batch_imgs).reshape(
+            len(cur_batch_imgs), IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL)
+        y = to_categorical(
+            cur_batch_labels, num_classes=CLASS_NUM)
+
+        dataset[batchIdx] = shuffle_dataset(x, y)
+
+    return dataset
+
+
+def dataAugment(x, y, batch_size=256):
+    aug_gen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+    )
+
+    aug_gen.fit(x)
+    gen = aug_gen.flow(x, y, batch_size=batch_size)
+
+    return gen
 
 
 def shuffle_dataset(x, y):
--- src/training-scripts/py/cifar10/main.py
+++ src/training-scripts/py/cifar10/main.py
@@ -1,29 +1,10 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-# from tensorflow.keras.datasets import cifar10
-# from tensorflow.keras.utils import to_categorical
-from dataHandler import load_data, CLASS_NUM
+from dataHandler import load_data, CLASS_NUM, dataAugment
+from model import getModel
 
 print('Loading data...')
 train_imgs, train_labels = load_data(type='TRAIN')
 test_imgs, test_labels = load_data(type='TEST')
-# (train_imgs, train_labels), (test_imgs, test_labels) = cifar10.load_data()
-
-# train_labels = train_labels.reshape(train_labels.shape[0])
-# test_labels = test_labels.reshape(test_labels.shape[0])
-
-# print('X_train shape:', train_imgs.shape)
-# print(train_imgs.shape[0], 'training samples')
-# print(test_imgs.shape[0], 'validation samples')
-
-# train_imgs = train_imgs.astype('float32')
-# test_imgs = test_imgs.astype('float32')
-# train_imgs /= 255
-# test_imgs /= 255
-
-# train_labels = to_categorical(train_labels, CLASS_NUM)
-# test_labels = to_categorical(test_labels, CLASS_NUM)
 
 print('Data loaded.')
 # print(train_imgs[0:1], train_labels.shape)
@@ -33,59 +14,10 @@ BATCH_SIZE = 256
 EPOCH = 100
 
 
-def getModel():
-    model = models.Sequential()
-
-    # stage 1
-    model.add(layers.Conv2D(filters=64, strides=1, kernel_size=KERNEL_SIZE, activation='relu',
-                            input_shape=train_imgs.shape[1:], kernel_initializer='he_normal', padding='same'))
-
-    model.add(layers.Conv2D(filters=64, strides=1, kernel_size=KERNEL_SIZE, activation='relu',
-                            kernel_initializer='he_normal', padding='same'))
-
-    model.add(layers.MaxPooling2D(pool_size=2, strides=2, padding='same'))
-
-    # stage 2
-    model.add(layers.Conv2D(filters=128, strides=1, kernel_size=KERNEL_SIZE, activation='relu',
-                            kernel_initializer='he_normal', padding='same'))
-
-    model.add(layers.Conv2D(filters=128, strides=1, kernel_size=KERNEL_SIZE, activation='relu',
-                            kernel_initializer='he_normal', padding='same'))
-
-    model.add(layers.MaxPooling2D(pool_size=2, strides=2, padding='same'))
-
-    # stage 3
-    model.add(layers.Conv2D(filters=256, strides=1, kernel_size=KERNEL_SIZE, activation='relu',
-                            kernel_initializer='he_normal', padding='same'))
-
-    model.add(layers.Conv2D(filters=256, strides=1, kernel_size=KERNEL_SIZE, activation='relu',
-                            kernel_initializer='he_normal', padding='same'))
-
-    model.add(layers.MaxPooling2D(pool_size=2, strides=2, padding='same'))
-
-    # flatten as one dimension
-    model.add(layers.Flatten())
-
-    # fully connected layer 500 neurons
-    model.add(layers.Dense(units=128, activation='relu'))
-
-    model.add(layers.Dropout(0.5))
-    
-    # final fully connected layer CLASS_NUM neurons with respect to CLASS_NUM subjects
-    model.add(layers.Dense(units=CLASS_NUM, activation='softmax',
-              kernel_initializer='he_normal'))
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    return model
-
-
 def train():
     gen = dataAugment(train_imgs, train_labels, batch_size=BATCH_SIZE)
 
-    model = getModel()
+    model = getModel(train_imgs.shape[1:], KERNEL_SIZE, CLASS_NUM, reg=True)
 
     model.summary()
 
@@ -94,20 +26,19 @@ def train():
     tensorboard_callback = tf.keras.callbacks.TensorBoard(
         log_dir=log_dir, histogram_freq=1)
 
-    def scheduler(epoch, lr):
-        if (epoch == 1):
-            return 0.01
-        if epoch % 10 == 0:
-            return lr - 0.0005
-        else:
-            return lr
+    # def scheduler(epoch, lr):
+    #     if (epoch == 1):
+    #         return 0.01
+    #     if epoch % 10 == 0:
+    #         return lr - 0.0005
+    #     else:
+    #         return lr
 
-    learn_scheduler_callback = tf.keras.callbacks.LearningRateScheduler(
-        scheduler)
+    # learn_scheduler_callback = tf.keras.callbacks.LearningRateScheduler(
+    #     scheduler)
 
     h = model.fit(x=gen,  epochs=EPOCH, steps_per_epoch=50000 // BATCH_SIZE,
                   validation_data=(test_imgs, test_labels), callbacks=[tensorboard_callback])
-    # steps_per_epoch=50000//BATCH_SIZE
 
     # model.fit(train_imgs, train_labels, epochs=EPOCH, batch_size=BATCH_SIZE, shuffle=True,
     #           validation_data=(test_imgs, test_labels),
@@ -115,29 +46,6 @@ def train():
 
     # model.save('CIFAR10_model_with_data_augmentation_dual_GPU.h5')
     # validation_data=(test_imgs, test_labels),
-
-
-def dataAugment(x, y, batch_size=256):
-    aug_gen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        # randomly rotate images in the range (degrees, 0 to 180)
-        rotation_range=0,
-        # randomly shift images horizontally (fraction of total width)
-        width_shift_range=0.1,
-        # randomly shift images vertically (fraction of total height)
-        height_shift_range=0.1,
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False,  # randomly flip images
-    )
-
-    aug_gen.fit(x)
-    gen = aug_gen.flow(x, y, batch_size=batch_size)
-
-    return gen
 
 
 train()
