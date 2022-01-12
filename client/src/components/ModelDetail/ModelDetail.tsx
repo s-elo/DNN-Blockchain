ba502e6f6fe3ff8f0c94f967fcf92b1dc95cb53d
s-elo <zz321yy@qq.com>
2022-01-12 11:15:40 +0800

    download according to the provided address

--- client/src/components/ModelDetail/ModelDetail.tsx
+++ client/src/components/ModelDetail/ModelDetail.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 
 // import { Model } from "../ModelItem/ModelItem";
 import "./ModelDetail.less";
 
 const iframe = document.createElement("iframe");
-iframe.style.display = 'none';
+iframe.style.display = "none";
 const body = document.body;
 body.appendChild(iframe);
 
@@ -14,14 +14,25 @@ export default function ModelDetail(
 ) {
   const { modelName } = props.match.params;
 
+  const [address, setAddress] = useState("");
+  const handleAddressChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setAddress(e.target.value);
+
   const getScript = () => {
-    iframe.src = `http://localhost:3500/get-scripts/${modelName}-py`;
+    iframe.src = `http://localhost:3500/get-scripts/${modelName}-py?address=${address}`;
   };
 
   return (
     <>
       <title>{modelName}</title>
       <article>{`this is the desc`}</article>
+      <input
+        type="text"
+        className="input"
+        value={address}
+        placeholder="please provide your account address here"
+        onChange={handleAddressChange}
+      />
       <button onClick={getScript} className="btn download-btn">
         Download Script
       </button>
