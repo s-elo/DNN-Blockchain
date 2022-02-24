0dfa40fcac811bb97318c3da634e71383785d83d
s-elo <zz321yy@qq.com>
2022-02-24 16:10:07 +0800

    cifar100 initialization

---
+++ acc_simul/cifar100/dataHandler.py
@@ -0,0 +1,69 @@
+import numpy as np
+from random import shuffle
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+CLASS_NUM = 20
+
+
+def load_remote(split_num=1):
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+    # do the shuffle
+    x_train, y_train = shuffle_dataset(x_train, y_train)
+    x_test, y_test = shuffle_dataset(x_test, y_test)
+
+    x_train = x_train / 255
+    x_test = x_test / 255
+
+    y_train = to_categorical(y_train, num_classes=CLASS_NUM)
+    y_test = to_categorical(y_test, num_classes=CLASS_NUM)
+
+    # split the training data
+    train_split_data = list(
+        zip(np.split(x_train, split_num, axis=0), np.split(y_train, split_num, axis=0)))
+
+    return (train_split_data, x_test, y_test, x_train, y_train)
+
+
+def dataAugment(x, y, batch_size=256):
+    aug_gen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+    )
+
+    aug_gen.fit(x)
+    gen = aug_gen.flow(x, y, batch_size=batch_size)
+
+    return gen
+
+
+def shuffle_dataset(x, y):
+    ind_list = [i for i in range(y.shape[0])]
+    shuffle(ind_list)
+    x_new = x[ind_list, :, :, :]
+    y_new = y[ind_list, ]
+
+    return (x_new, y_new)
+
+
+if __name__ == '__main__':
+    arr1 = np.array([[1, 2], [2, 3], [4, 5], [6, 7]])
+    arr2 = np.array([[1], [2], [3], [4]])
+    # x_shuffle, y_shuffle = shuffle_dataset(arr1, arr2)
+    # print(x_shuffle, y_shuffle)
+    print(np.split(arr1, 1, axis=0), np.split(arr2, 1, axis=0))
+    print(list(zip(arr1, arr2)))
+    load_remote()
---
+++ acc_simul/cifar100/fl_simul.py
@@ -0,0 +1,137 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from dataHandler import dataAugment, CLASS_NUM, load_remote
+from model import getModel
+import shutil
+import os
+
+shutil.rmtree('./ret_img')
+os.mkdir('./ret_img')
+
+KERNEL_SIZE = 3
+BATCH_SIZE = 256
+EPOCH = 20
+ROUND = 10
+USER_NUM = 5
+
+print('Loading data...')
+dataset, test_imgs, test_labels, _, _ = load_remote(USER_NUM)
+print('Data loaded.')
+print(len(dataset), dataset[0][0].shape, dataset[0][1].shape)
+
+users_acc = [[]]*USER_NUM
+users_val_acc = [[]]*USER_NUM
+users_loss = [[]]*USER_NUM
+users_val_loss = [[]]*USER_NUM
+
+overall_val_acc = []*ROUND
+overall_val_loss = []*ROUND
+
+model = getModel(test_imgs.shape[1:], KERNEL_SIZE, CLASS_NUM)
+
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+
+def split_train(model, dataset, test_imgs, test_labels):
+    weights = model.get_weights()
+
+    new_weights = []
+    for batchIdx in range(0, len(dataset)):
+        print('\n')
+        print('================User' + str(batchIdx + 1) + '=================')
+
+        train_imgs = dataset[batchIdx][0]
+        train_labels = dataset[batchIdx][1]
+
+        gen = dataAugment(train_imgs, train_labels, batch_size=BATCH_SIZE)
+
+        model.set_weights(weights)
+
+        h = model.fit(x=gen,  epochs=EPOCH, steps_per_epoch=train_imgs.shape[0] // BATCH_SIZE,
+                      validation_data=(test_imgs, test_labels))
+
+        acc = h.history['accuracy']
+        loss = h.history['loss']
+        val_acc = h.history['val_accuracy']
+        val_loss = h.history['val_loss']
+
+        users_acc[batchIdx] = users_acc[batchIdx] + acc
+
+        users_val_acc[batchIdx] = users_val_acc[batchIdx] + val_acc
+        users_loss[batchIdx] = users_loss[batchIdx] + loss
+        users_val_loss[batchIdx] = users_val_loss[batchIdx] + val_loss
+
+        new_weights.append(model.get_weights())
+
+    return new_weights
+
+
+def fedAvg(model, new_weights=[]):
+    sum_weights = 0
+
+    for i in range(0, len(new_weights)):
+        sum_weights += np.array(new_weights[i], dtype=object)
+
+    mean_weights = sum_weights / len(new_weights)
+
+    model.set_weights(mean_weights.tolist())
+    return model
+
+
+def fl():
+    avg_model = model
+
+    for i in range(0, ROUND):
+        print('\n')
+        print('================Round' + str(i + 1) + '=================')
+
+        new_weights = split_train(avg_model, dataset, test_imgs, test_labels)
+        avg_model = fedAvg(model, new_weights)
+
+        print('Evaluation')
+        loss, acc = avg_model.evaluate(test_imgs, test_labels)
+
+        overall_val_acc.append(acc)
+        overall_val_loss.append(loss)
+
+    for user in range(0, USER_NUM):
+        plt.figure()
+        plt.title("user" + str(user + 1) + " accuracy along rounds")
+        plt.plot(users_acc[user], label='user_acc' + str(user + 1))
+        plt.plot(users_val_acc[user], label='user_val_acc' + str(user + 1))
+        plt.xlabel('epoch')
+        plt.ylabel('accuracy')
+        plt.legend()
+        plt.savefig('./ret_img/user' + str(user + 1) + '_acc.png')
+
+        plt.figure()
+        plt.title("user" + str(user + 1) + " loss along rounds")
+        plt.plot(users_loss[user], label='user_loss' + str(user + 1))
+        plt.plot(users_val_loss[user], label='user_val_loss' + str(user + 1))
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.legend()
+        plt.savefig('./ret_img/user' + str(user + 1) + '_loss.png')
+
+    plt.figure()
+    plt.plot(overall_val_acc, label='overall accuracy')
+    plt.xlabel('round')
+    plt.ylabel('accuracy')
+    plt.legend()
+    plt.savefig('./ret_img/overall_acc.png')
+
+    plt.figure()
+    plt.plot(overall_val_loss, label='overall loss')
+    plt.xlabel('round')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.savefig('./ret_img/overall_loss.png')
+
+
+fl()
+plt.show()
